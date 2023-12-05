ec065b39002e7d1b4b30e74017e5c9f327e382c0
Rajesh Kumar Yadav <81347109+rajesh4289@users.noreply.github.com>
2023-12-05 23:39:50 +0530

    importing tkinter and other libraries

---
+++ Ludo.py
@@ -0,0 +1,7 @@
+                                                        # Ludo game Integrated With AI #
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image,ImageTk
+import time
+from random import randint, choice
+
