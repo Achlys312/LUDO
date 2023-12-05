6fc7aefb800d273e2d9d41cd2fe6bc335fd4b90e
Rajesh Kumar Yadav <81347109+rajesh4289@users.noreply.github.com>
2023-12-05 23:43:25 +0530

    Number to room to be traverse by specific color coin, store in that variable

--- Ludo.py
+++ Ludo.py
@@ -28,7 +28,7 @@ class Ludo:
 
         # Ludo block all side image store
         self.block_number_side = [one_side_block, two_side_block, three_side_block, four_side_block, five_side_block, six_side_block]
-        
+
         # Use for store specific position of all coins
         self.red_coord_store = [-1, -1, -1, -1]
         self.green_coord_store = [-1, -1, -1, -1]
@@ -45,3 +45,41 @@ class Ludo:
             self.green_coin_position[index] = -1
             self.yellow_coin_position[index] = -1
             self.sky_blue_coin_position[index] = -1
+
+        # Number to room to be traverse by specific color coin, store in that variable
+        self.move_red_counter = 0
+        self.move_green_counter = 0
+        self.move_yellow_counter = 0
+        self.move_sky_blue_counter = 0
+
+        self.take_permission = 0
+        self.six_with_overlap = 0
+
+        self.red_store_active = 0
+        self.sky_blue_store_active = 0
+        self.yellow_store_active = 0
+        self.green_store_active = 0
+
+        self.six_counter = 0
+        self.time_for = -1
+
+        # Some variables initializes with None
+        self.right_star = None
+        self.down_star = None
+        self.left_star = None
+        self.up_star = None
+
+        # Robo Control
+        self.robo_prem = 0
+        self.count_robo_stage_from_start = 0
+        self.robo_store = []
+
+        # By default some function call
+        self.board_set_up()
+
+        self.instruction_btn_red()
+        self.instruction_btn_sky_blue()
+        self.instruction_btn_yellow()
+        self.instruction_btn_green()
+
+        self.take_initial_control()
