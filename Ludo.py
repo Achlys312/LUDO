9ede0aa48461027452ce8f2969dd971c58e433c0
Kartik Dhoundiyal <kartikdhoundiyal2491@gmail.com>
2023-12-06 01:29:22 +0530

    adding coord_overlap

--- Ludo.py
+++ Ludo.py
@@ -1141,4 +1141,5 @@ class Ludo:
             if  int(counter_coin) + int(path_counter) <= 106:
                 counter_coin = self.room_sky_blue_traversal(specific_coin, number_label, number_label_x, number_label_y,path_counter,counter_coin)
 
+        return counter_coin
                 
