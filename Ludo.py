74fab27e27ca21d5d24f9599582d8bd7c0b4cdf0
Kartik Dhoundiyal <kartikdhoundiyal2491@gmail.com>
2023-12-06 01:28:16 +0530

    adding coord_overlap

--- Ludo.py
+++ Ludo.py
@@ -1124,4 +1124,9 @@ class Ludo:
 
 
     def under_room_traversal_control(self,specific_coin,number_label,number_label_x,number_label_y,path_counter,counter_coin,color_coin):          
+
+        if color_coin == "red" and counter_coin >= 100:
+            if int(counter_coin)+int(path_counter)<=106:
+               counter_coin = self.room_red_traversal(specific_coin, number_label, number_label_x, number_label_y, path_counter, counter_coin)
+
         
