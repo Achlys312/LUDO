6436b542922176dd2888f93e60ec14bcac823794
Rajesh Kumar Yadav <81347109+rajesh4289@users.noreply.github.com>
2023-12-05 23:41:27 +0530

    making canvas

--- Ludo.py
+++ Ludo.py
@@ -5,3 +5,26 @@ from PIL import Image,ImageTk
 import time
 from random import randint, choice
 
+class Ludo:
+    def __init__(self, root,six_side_block,five_side_block,four_side_block,three_side_block,two_side_block,one_side_block):
+        self.window = root
+        # Make canvas
+        self.make_canvas = Canvas(self.window, bg="#141414", width=800, height=630)
+        self.make_canvas.pack(fill=BOTH,expand=1)
+
+        # Make some containers to store data
+        self.made_red_coin = []
+        self.made_green_coin = []
+        self.made_yellow_coin = []
+        self.made_sky_blue_coin = []
+
+        self.red_number_label = []
+        self.green_number_label = []
+        self.yellow_number_label = []
+        self.sky_blue_number_label = []
+
+        self.block_value_predict = []
+        self.total_people_play = []
+
+        # Ludo block all side image store
+        self.block_number_side = [one_side_block, two_side_block, three_side_block, four_side_block, five_side_block, six_side_block]
